07a10f123bbeb53f7c2c3185058dfc82ed2670a8
Guanhao Yin <sopium@mysterious.site>
2019-09-28 03:08:19 +0800

    Use clap through structopt

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,13 @@
 // You should have received a copy of the GNU General Public License
 // along with TiTun.  If not, see <https://www.gnu.org/licenses/>.
 
-use clap::{App, Arg};
 #[cfg(windows)]
 use failure::bail;
 use failure::Error;
 use log::info;
+#[cfg(windows)]
+use std::net::Ipv4Addr;
+use structopt::{clap::crate_version, StructOpt};
 use titun::run::*;
 
 fn main() {
@@ -29,67 +31,65 @@ fn main() {
     });
 }
 
+#[cfg(windows)]
+fn parse_network(network: &str) -> Result<(Ipv4Addr, u32), Error> {
+    let parts: Vec<_> = network.split('/').take(3).collect();
+    if parts.len() != 2 {
+        bail!("Invalid network: {}", network);
+    }
+    let addr: ::std::net::Ipv4Addr = parts[0].parse()?;
+    let prefix = parts[1].parse()?;
+    if prefix > 32 {
+        bail!("Invalid network: {}", network);
+    }
+    Ok((addr, prefix))
+}
+
+#[derive(StructOpt)]
+#[structopt(rename_all = "kebab")]
+struct Options {
+    #[structopt(short, long, help = "Run in foreground (don't daemonize)")]
+    foreground: bool,
+
+    #[structopt(long, help = "Exit if stdin is closed")]
+    exit_stdin_eof: bool,
+
+    #[cfg(windows)]
+    #[structopt(
+        long,
+        value_name = "IP/PREFIX_LEN",
+        parse(try_from_str = parse_network),
+        help = "Network configuration for the device",
+    )]
+    network: (Ipv4Addr, u32),
+
+    #[structopt(value_name = "DEVICE_NAME", help = "Device name")]
+    dev: String,
+}
+
 fn real_main() -> Result<(), Error> {
     env_logger::init();
 
     let version = if !env!("GIT_HASH").is_empty() {
-        concat!(clap::crate_version!(), "-", env!("GIT_HASH"))
+        concat!(crate_version!(), "-", env!("GIT_HASH"))
     } else {
-        clap::crate_version!()
+        crate_version!()
     };
 
-    let app = App::new("titun")
-        .version(version)
-        .about(include_str!("copyright.txt"))
-        .arg(
-            Arg::with_name("dev")
-                .value_name("DEVICE_NAME")
-                .required(true)
-                .help("Device name"),
-        )
-        .arg(
-            Arg::with_name("foreground")
-                .short("f")
-                .long("foreground")
-                .help("Run in foreground (don't daemonize)"),
-        )
-        .arg(
-            Arg::with_name("exit-stdin-eof")
-                .long("exit-stdin-eof")
-                .help("Exit if stdin is closed"),
-        );
-    #[cfg(windows)]
-    let app = app.arg(
-        Arg::with_name("network")
-            .long("network")
-            .value_name("IP/PREFIX_LEN")
-            .required(true)
-            .help("Network configuration for the device"),
+    let options = Options::from_clap(
+        &Options::clap()
+            .about(include_str!("copyright.txt"))
+            .version(version)
+            .get_matches(),
     );
 
-    let m = app.get_matches();
-
     info!("titun {}", version);
-    #[cfg(windows)]
-    let network = {
-        let network = m.value_of("network").unwrap();
-        let parts: Vec<_> = network.split('/').take(3).collect();
-        if parts.len() != 2 {
-            bail!("Invalid network: {}", network);
-        }
-        let addr: ::std::net::Ipv4Addr = parts[0].parse()?;
-        let prefix = parts[1].parse()?;
-        if prefix > 32 {
-            bail!("Invalid network: {}", network);
-        }
-        (addr, prefix)
-    };
     let config = Config {
-        dev_name: m.value_of("dev").unwrap().to_string(),
-        exit_stdin_eof: m.is_present("exit-stdin-eof"),
+        dev_name: options.dev,
+        exit_stdin_eof: options.exit_stdin_eof,
         #[cfg(windows)]
-        network,
-        daemonize: !m.is_present("foreground"),
+        network: options.network,
+        daemonize: !options.foreground,
     };
     let threads = if let Ok(Ok(t)) = std::env::var("TITUN_THREADS").map(|t| t.parse()) {
         t
