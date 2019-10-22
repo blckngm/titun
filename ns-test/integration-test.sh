d0b808b120ed3650c0b5752281374bd1d5bf769c
Yin Guanhao <sopium@mysterious.site>
2019-10-22 14:17:00 +0800

    Use setconf in integration test
    And always handle SIGHUPs.

--- ns-test/integration-test.sh
+++ ns-test/integration-test.sh
@@ -11,6 +11,8 @@ clean_up() {
 }
 trap clean_up EXIT
 
+KCOV_TITUN='kcov --verify --exclude-pattern=/usr/include,/lib,/usr/lib,/.cargo,patched/ring ../coverage/$(dd if=/dev/urandom of=/dev/stdout bs=6 count=1 2>/dev/null | base64 | tr / _) ../target/debug/titun'
+
 # Setup.
 
 ip netns add titun-test-1
@@ -27,11 +29,13 @@ ip -n titun-test-2 addr add 192.168.3.2/24 dev eth2
 
 mkdir -p ../coverage
 
-ip netns exec titun-test-1 kcov --verify --exclude-pattern=/usr/lib,/.cargo,patched/ring ../coverage/tun1 ../target/debug/titun -fc tun1.conf tun1 &
-ip netns exec titun-test-2 kcov --verify --exclude-pattern=/usr/lib,/.cargo,patched/ring ../coverage/tun2 ../target/debug/titun -fc tun2.conf tun2 &
+eval ip netns exec titun-test-1 $KCOV_TITUN -fc tun1.conf tun1 &
+eval ip netns exec titun-test-2 $KCOV_TITUN -f tun2 &
 
 while [ ! -e /var/run/wireguard/tun1.sock ]; do sleep 2; done
 while [ ! -e /var/run/wireguard/tun2.sock ]; do sleep 2; done
+
+wg setconf tun2 tun2.conf
 
 ip -n titun-test-1 link set tun1 up mtu 59000
 ip -n titun-test-1 addr add 192.168.77.1/24 dev tun1
@@ -42,7 +46,7 @@ ip -n titun-test-2 addr add 192.168.77.2/24 dev tun2
 ip netns exec titun-test-2 ping -i 0.1 -c 5 192.168.77.1
 
 # Show.
-kcov --verify --exclude-pattern=/usr/lib,/.cargo,patched/ring ../coverage/show ../target/debug/titun show
+eval $KCOV_TITUN show
 
 # Generate and add PSK to config files.
 PSK=$(../target/debug/titun genpsk)
@@ -51,10 +55,11 @@ echo PresharedKey = $PSK >> tun2.conf
 
 # Reload.
 pkill -HUP titun
+wg setconf tun2 tun2.conf
 sleep 1
 
 # Show again.
-kcov --verify --exclude-pattern=/usr/lib,/.cargo,patched/ring ../coverage/show2 ../target/debug/titun show tun1 tun2
+eval $KCOV_TITUN show tun1 tun2
 
 # And ping again.
 ip netns exec titun-test-2 ping -i 0.1 -c 5 192.168.77.1
