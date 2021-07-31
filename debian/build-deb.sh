e077d94ab23c5c0d4a2b05856605f315dcff8edd
Yin Guanhao <sopium@mysterious.site>
2021-07-31 21:34:51 +0800

    v0.2.1

--- debian/build-deb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cross build --release --target=x86_64-unknown-linux-musl
-mkdir -p package/usr/bin/
-cp ../target/x86_64-unknown-linux-musl/release/titun package/usr/bin/titun
-trap "rm package/usr/bin/titun" 0
-fakeroot dpkg-deb --build package titun.deb
